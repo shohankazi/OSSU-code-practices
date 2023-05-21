d81568b7f4850765aa2ea5dbfc6229f768ca3b18
shohankazi <shohankazi012@gmail.com>
2023-05-21 20:37:50 +0600

    code added"

---
+++ Python for everybody/conditionals.py
@@ -0,0 +1,9 @@
+#Indentation
+"""
+==> Increase indent indent after an if statement or for statement (after : )
+==> Maintain indent to indicate the scope of the block (which lines are affected by the if/or)
+==> Reduce indent back to the level of the if statement or for statement to indicate the end of the block
+==> Blank lines are ignored - they do not affect indentation
+==> Comments on a line by themselves are ignored with regard to indentation
+
+"""
---
+++ Python for everybody/hello.py
@@ -0,0 +1,4 @@
+x = 1 + 2 * 3 - 8 / 4
+y = int(98.6)
+print(x)
+print(y)
