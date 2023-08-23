4d10c11ce4effd07c57787b29287de9ed6156e93
shohankazi <shohankazi012@gmail.com>
2023-08-23 22:27:15 +0600

    finding the smallest number in python

--- Python for everybody/loop_practise.py
+++ Python for everybody/loop_practise.py
@@ -1,6 +1,6 @@
 # Finding the largest value
-"""
-largest = 0
+
+""" largest = 0
 print('Before',largest)
 for num in [9,41,12,3,74,15] :
     if num > largest :
@@ -19,13 +19,49 @@ for thing in [9,41,12,3,74,15] :
     print(thing, " + ",zork)
 print('After',zork)
 """
+
 # Finding the average in a loop
 
+"""
 count = 0
 sum = 0
 print('Before',count,sum)
 for value in [9,41,12,3,74,15] :
     count = count + 1
     sum = sum + value
+    average = sum / count
     print(count,sum,value)
-print('After',count,sum,sum/count)
+print('After',count,sum,average)
+"""
+
+# Filtering in a loop
+
+""" print('Before')
+for value in [9,41,12,3,74,15] :
+    if value > 20 :
+        print('Large number',value)
+print('After') """
+
+# Search Using a Boolean Variable
+
+""" found = False
+print('Before', found)
+for value in [9,41,12,3,74,15] :
+    if value == 3 :
+        found = True
+    else :
+        found = False
+    print(found,value)
+print('After',found) """
+
+# Finding the smallest value
+
+smallest = None
+print('Before')
+for value in [9,41,12,3,74,15] :
+    if smallest is None :
+        smallest = value
+    elif value < smallest :
+        smallest = value
+    print(smallest,value)
+print('After',smallest)
