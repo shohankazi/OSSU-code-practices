e650f4061904972e08b5c29f9f8294e123e46e0a
shohankazi <shohankazi012@gmail.com>
2023-08-24 11:59:10 +0600

    String slicing part understood

---
+++ Python for everybody/6_String_Slicing.py
@@ -0,0 +1,9 @@
+# Slicing Strings
+# We can also look at any continuous section of a string using a colon operator
+# The second number is one beyond the end of the slice "up to but not including"
+# If the second number is beyond the end of the string, it stops at the end
+
+str = 'Monty Python'
+print(str[0:4])
+print(str[6:7])
+print(str[6:20])
