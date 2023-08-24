7d1fb54aab53b229d757a2c2eb90250cb157225a
shohankazi <shohankazi012@gmail.com>
2023-08-24 12:05:05 +0600

    String eliminating and concatenation topic cleared

--- Python for everybody/6_String_Slicing.py
+++ Python for everybody/6_String_Slicing.py
@@ -3,7 +3,25 @@
 # The second number is one beyond the end of the slice "up to but not including"
 # If the second number is beyond the end of the string, it stops at the end
 
+""" 
 str = 'Monty Python'
 print(str[0:4])
 print(str[6:7])
-print(str[6:20])
+print(str[6:20]) 
+"""
+
+# Eliminating
+""" 
+print(str[:2])
+print(str[8:])
+print(str[:]) 
+"""
+
+# String Concatenation
+# When the + operator is applied to strings, it means "Concatenation"
+
+""" a = 'Hello'
+b = a + 'There'
+print(b)
+c = a + ' ' + 'There'
+print(c) """
