4de2a628d3a529a08c6a2975b3fe926a4f41547b
shohankazi <shohankazi012@gmail.com>
2023-08-26 13:51:49 +0600

    range function, concatenation using + , list slicing, list method concept cleared

--- Python for everybody/8_list.py
+++ Python for everybody/8_list.py
@@ -49,12 +49,52 @@
 1. Strings are "immutable = unchangeable" - we cannot change the contents of a string - we must make a new string to make any change.
 2. Lists are "mutable = Changeable" - we can change an element of a list using the index operator.
 """
-fruit = 'Banana'
+# fruit = 'Banana'
 # fruit[0] = 'b'
 # print(fruit) # it will show traceback error.
 
-x = fruit.lower()
-print(x)
-lotto = [2,14,26,41,68]
-lotto[2] = 28
-print(lotto)
+# x = fruit.lower()
+# print(x)
+# lotto = [2,14,26,41,68]
+# lotto[2] = 28
+# print(lotto)
+
+# How long a list?
+""" 
+1. The len() function takes a list as a parameter and returns the number of elements in the list.
+2. Actually len() tells us the number of elements of any set or sequence(such as a string...)
+"""
+# greet = 'Hello Bob'
+# print(len(greet))
+# x = [1,2,'joe',99]
+# print(len(x))
+
+# Using the range function
+""" 
+1. The range function returns a list of numbers that range from zero to one less than parameter.
+2. We can construct an index loop using for and an integer iterator
+"""
+# print(range(4))
+# friends = ['Joseph', 'Glenn', 'Sally']
+# print(len(friends))
+# print(range(len(friends)))
+
+# Concatenating lists using +
+""" We can create a new list by adding two existing lists together """
+# a = [1,2,3]
+# b = [4,5,6]
+# c = a + b
+# print(c)
+
+# Lists can be sliced using ':'
+# t = [9,41,12,3,74,15]
+# s = t[1:3]
+# st = t[:5]
+# s = t[3:]
+# all = t[:]
+# print(s,st,all)
+
+# List methods
+# x = list()
+# print(type(x))
+# print(dir(x))
