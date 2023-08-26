a736132b31ce1a936c832c3e6a471d2a3f4982ba
shohankazi <shohankazi012@gmail.com>
2023-08-26 23:34:01 +0600

    Dictionary part two completed

--- Python for everybody/9_dict.py
+++ Python for everybody/9_dict.py
@@ -62,7 +62,59 @@
 1. Dictionary literals use curly braces and have a list of key:value pairs
 2. You can make an empty dictionary using empty curly braces.
 """
-jjj = {'chuck': 1, 'fred': 42,'jan':100}
-print(jjj)
-ooo = { }
-print(ooo)
+# jjj = {'chuck': 1, 'fred': 42,'jan':100}
+# print(jjj)
+# ooo = { }
+# print(ooo)
+
+# Many counters with dictionary
+""" One common use of dictionaries is counting how often we "see" something """
+
+# ccc = dict()
+# ccc['csev'] = 1
+# ccc['cwen'] = 1
+# print(ccc)
+# ccc['cwen'] = ccc['cwen'] + 1
+# print(ccc)
+
+# Dictionary Tracebacks
+"""
+1. It is an error to reference a key which is not in the dictionary.
+2. We can use the in operator to see if a key is in the dictionary. 
+"""
+# ccc = dict()
+# print(ccc['csev'])
+# print('csev' in ccc)
+
+# When we see a new name
+""" 
+When we encounter a new name, we need to add a new entry in the dictionary and if the second or later time we have seen the name, we simply add one to the count in the dictionary under that name.
+"""
+# counts = dict()
+# names = ['csev','cwen','csev','zqian','cwen']
+# for name in names :
+#     if name not in counts:
+#         counts[name] = 1
+#         # print(counts)
+#     else:
+#         counts[name] = counts[name] + 1
+# print(counts)
+
+# The get method for dictionaries
+""" The pattern of checking to see if a key is already in a dictionary and assuming a default value if the key is not there is so common that there is a method called get() that does this for us. Default value if key does not exist(and no traceback)"""
+
+# if name in counts:
+#     x = counts[name]
+# else :
+#     x = 0
+# x = counts.get(name,0)
+# print(x)
+
+# Simplified counting with get()
+""" We can use get() and provide a default value of zero when the key is not yet in the dictionary - and then just add one """
+
+counts = dict()
+names = ['csev','cwen','csev','zqian','cwen']
+for name in names :
+    counts[name] = counts.get(name,0) + 1
+print(counts)
