ade71fbe959da541058303081d6676537876eb40
shohankazi <shohankazi012@gmail.com>
2023-08-29 00:22:12 +0600

    using urllib in python part complete

--- Python for everybody/12_network_programming.py
+++ Python for everybody/12_network_programming.py
@@ -129,3 +129,32 @@ Protocol         host      document
 2. When we read data from an external resource, we must decode it based on the character set so it is properly represented in Python 3 as a string.
 """
 
+# Using urllib in python
+""" Since HTTP is so common, we have a library that does all the socket work for us and makes web pages look like a file """
+
+# import urllib.request, urllib.parse, urllib.error
+
+# fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+# for line in fhand:
+#     print(line.decode().strip())
+    
+
+# Like a file
+
+# import urllib.request, urllib.parse, urllib.error
+# fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+
+# counts = dict()
+# for line in fhand:
+#     words = line.decode().split()
+#     for word in words:
+#         counts[word] = counts.get(word,0) + 1
+# print(counts)
+
+# Reading web pages
+
+# import urllib.request, urllib.parse, urllib.error
+
+# fhand = urllib.request.urlopen('http://www.dr-chuck.com/page1.html')
+# for line in fhand:
+#     print(line.decode().strip())
