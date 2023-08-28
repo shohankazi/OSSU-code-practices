30914e045c1f072db9bf2d33de889fc8a07d7da5
shohankazi <shohankazi012@gmail.com>
2023-08-28 22:18:31 +0600

    Using HTTP in Python part complete

--- Python for everybody/12_network_programming.py
+++ Python for everybody/12_network_programming.py
@@ -77,3 +77,19 @@ Protocol         host      document
 3. www.ietf.org
 4. Standards are called "RFC's" - "Request for Comments"
 """
+
+# An HTTP request in python
+
+import socket
+
+mysock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+mysock.connect(('data.pr4e.org',80))
+cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\n\n'.encode()
+mysock.send(cmd)
+
+while True:
+    data = mysock.recv(512)
+    if (len(data)<1):
+        break
+    print(data.decode())
+mysock.close()
