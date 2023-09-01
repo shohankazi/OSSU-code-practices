21c9f779ca6f27a7cb75b11b96e66c406667afa2
shohankazi <shohankazi012@gmail.com>
2023-09-01 19:00:34 +0600

    visualization - geodata - part 1

---
+++ Python for everybody/16_Data-visualization.py
@@ -0,0 +1,16 @@
+# Many data mining technologies
+""" 
+1. https://hadoop.apache.org
+2. http://spark.apache.org
+3. https://aws.amazon.com/redshift/
+4. http://community.pentaho.com/
+"""
+# Personal Data Mining
+""" Our goal is to make you better programmers - not to make you data mining experts """
+# GeoData
+""" 
+1. Makes a google Map from user entered data
+2. Uses the google GeoData API
+3. Caches data in a database to avoid rate limiting and allow restarting
+4. Visualized in a browser using the Google Maps API
+"""
