6b38905bf21f8c7c8828d016fd4b07d2a50ce0a0
shohankazi <shohankazi012@gmail.com>
2023-09-01 23:31:23 +0600

    visualization - part 2 completed

--- Python for everybody/16_Data-visualization.py
+++ Python for everybody/16_Data-visualization.py
@@ -14,3 +14,45 @@
 3. Caches data in a database to avoid rate limiting and allow restarting
 4. Visualized in a browser using the Google Maps API
 """
+# Page Rank
+""" 
+1. Write a simple web page crawler
+2. Compute a simple version of Google's Page Rank algorithm
+3. Visualize the resulting network
+"""
+# Search Engine Architecture
+""" 
+1. Web Crawling
+2. Index Building
+3. Searching
+"""
+# Web Crawler
+""" A web crawler is a computer program that browses the world wide web in a methodical manner. Web crawlers are mainly used to create a copy of all the visited pages for later processing by a search engine that will index the downloaded pages to provide fast searches. 
+=>Retrieve a page
+=> Look through the page for links 
+=> Add the links to a list of "to be retrieved" sites
+=> Repeat
+"""
+# Web crawling Policy
+""" 
+=> A selection policy that states which pages to download
+=> A re-visit policy that states when to check for changes to the pages
+=> A politeness policy that states how to avoid overloading web sites, and
+=> A parallelization policy that states how to coordinate distributed web crawlers
+"""
+# Robots.txt
+""" 
+1. A way for a web site to communicate with web crawlers
+2. An informal and voluntary standard
+3. Sometimes folks make a "Spider Trap" to catch "bad" spiders
+"""
+# Google Architecture
+""" 
+1. Web Crawling
+2. Index Building
+3. Searching
+"""
+# Search indexing
+""" 
+Search engine indexing collects, parses, and stores data to facilitate fast and accurate information retrieval. The purpose of storing an index is to optimize speed and performance in finding relevant documents for a search query. Without an index, the search would scan every document in the corpus, which would require considerable time and computing power.
+"""
