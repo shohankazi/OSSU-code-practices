2144490d7471cf4d2fd75e5a0bbcd18ca259c479
shohankazi <shohankazi012@gmail.com>
2023-09-08 10:59:52 +0600

    OOP in Python completed

---
+++ Intro to CS and Programming using python/MitOpenCourseWare/Lecture-08 (OOP in Python).py
@@ -0,0 +1,20 @@
+# Object-Oriented Programming in Python:
+""" 
+1. Objects: An object is an instance of a class that has its own set of attributes and methods. Attributes are variables that store data, while methods are functions that operate on that data. An example of an object in Python could be a car. The car object might have attributes such as its make, model, and color, and methods such as start_engine() and stop_engine().
+
+2. Classes: A class is a blueprint for creating objects. It defines the attributes and methods that all instances of the class will have. An example of a class in Python could be a Person class. The Person class might have attributes such as name and age, and methods such as say_hello() and get_older().
+
+3. Methods: Methods are functions that are associated with a particular class. They can be used to modify the attributes of an object or to perform some other action. An example of a method in Python could be a calculate_area() method for a Rectangle class. The method might take the height and width of the rectangle as inputs and return the area of the rectangle.
+
+4. Attributes: Attributes are variables that store data associated with an object. They can be accessed and modified using methods. An example of an attribute in Python could be the length attribute of a Line class. The length attribute might store the length of the line and be accessed and modified using methods such as set_length() and get_length().
+
+5. Interacting with objects: Objects can interact with each other by calling each other's methods. For example, one object might call a method on another object to modify its attributes. An example of objects interacting with each other in Python could be a BankAccount object calling a transfer() method on another BankAccount object to transfer funds between accounts.
+
+6. Inheritance: Inheritance is a way of creating new classes that inherit attributes and methods from an existing class. This can be useful for creating subclasses that have some of the same functionality as their parent class, but with additional features. An example of inheritance in Python could be a Dog class that inherits from an Animal class. The Dog class might have additional attributes and methods specific to dogs, but would also inherit all the attributes and methods of the Animal class.
+
+7. Polymorphism: Polymorphism is the ability of objects to take on many forms. In Python, this is often achieved through method overloading, which allows different versions of a method to be applied to different types of objects. An example of polymorphism in Python could be a Shape class with different subclasses such as Circle, Square, and Triangle. Each subclass might have its own version of a draw() method that is specific to that shape.
+
+8. Encapsulation: Encapsulation is the practice of hiding the implementation details of a class from the outside world. This can make code more secure and easier to maintain. An example of encapsulation in Python could be a BankAccount class that hides the implementation details of how funds are stored and transferred from the outside world. The class might have methods such as deposit() and withdraw() that allow users to interact with the account without needing to know how it works internally.
+
+Throughout the video, the instructor provides examples and demonstrations of each of these topics to help illustrate how they work in practice.
+"""
