c8045ae921c6dbfa71147f82d86cb479e7890e1d
shohankazi <shohankazi012@gmail.com>
2023-09-11 23:16:55 +0600

    problem solved

--- Intro to CS and Programming using python/Edx/Python Problems.py
+++ Intro to CS and Programming using python/Edx/Python Problems.py
@@ -93,3 +93,89 @@ while iteration < 5:
         count += 1
     print("Iteration " + str(iteration) + "; count is: " + str(count))
     iteration += 1  """
+
+# Problem 10
+
+""" iteration = 0
+count = 0
+while iteration < 5:
+    # the variable 'letter' in the loop stands for every 
+    # character, including spaces and commas!
+    for letter in "hello, world": 
+        count += 1
+    print("Iteration " + str(iteration) + "; count is: " + str(count))
+    iteration += 1 """
+
+# Problem 11
+
+""" iteration = 0
+while iteration < 5:
+    count = 0
+    for letter in "hello, world":
+        count += 1
+    print("Iteration " + str(iteration) + "; count is: " + str(count))
+    iteration += 1 """
+
+# Problem 12
+
+""" iteration = 0
+while iteration < 5:
+    count = 0
+    for letter in "hello, world":
+        count += 1
+        if iteration % 2 == 0:
+            break
+    print("Iteration " + str(iteration) + "; count is: " + str(count))
+    iteration += 1  """
+    
+# Problem 13
+
+""" x = 25
+epsilon = 0.01
+step = 0.1
+guess = 0.0
+
+while guess <= x:
+    if abs(guess**2 -x) >= epsilon:
+        guess += step
+
+if abs(guess**2 - x) >= epsilon:
+    print('failed')
+else:
+    print('succeeded: ' + str(guess)) """
+    
+# Problem 14
+
+""" x = 25
+epsilon = 0.01
+step = 0.1
+guess = 0.0
+
+while abs(guess**2-x) >= epsilon:
+    if guess <= x:
+        guess += step
+    else:
+        break
+
+if abs(guess**2 - x) >= epsilon:
+    print('failed')
+else:
+    print('succeeded: ' + str(guess)) """
+    
+# Problem 15
+
+""" x = 23
+epsilon = 0.01
+step = 0.1
+guess = 0.0
+
+while abs(guess**2-x) >= epsilon:
+    if guess <= x:
+        guess += step
+    else:
+        break
+
+if abs(guess**2 - x) >= epsilon:
+    print('failed')
+else:
+    print('succeeded: ' + str(guess)) """
